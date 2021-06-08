f532ec22a191830e23c52473518903914d9fbf57
HaroldWalter <harold11walter@gmail.com>
2021-06-08 11:32:45 +0200

    creation des composents de pages

---
+++ frontRecettes/src/app/pages/details-recette/details-recette.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetailsRecetteComponent } from './details-recette.component';
+
+describe('DetailsRecetteComponent', () => {
+  let component: DetailsRecetteComponent;
+  let fixture: ComponentFixture<DetailsRecetteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetailsRecetteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetailsRecetteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
