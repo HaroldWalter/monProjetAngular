33eb1f7103a7c21f15c43eb523a4717f48e31dd7
HaroldWalter <harold11walter@gmail.com>
2021-06-09 11:56:14 +0200

    Creation de classes

---
+++ frontRecettes/src/app/pages/liste-recettes/miniature/miniature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-miniature',
+  templateUrl: './miniature.component.html',
+  styleUrls: ['./miniature.component.css']
+})
+export class MiniatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
