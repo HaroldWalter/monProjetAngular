1c1777b2ab9956f494935748c1876b05830a7b8a
HaroldWalter <harold11walter@gmail.com>
2021-06-11 12:19:50 +0200

    mise en place du pwa

--- frontRecettes/src/app/app.module.ts
+++ frontRecettes/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { MiniatureComponent } from './pages/liste-recettes/miniature/miniature.component';
 import { SlideComponent } from './pages/accueil/slide/slide.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -31,7 +33,13 @@ import { SlideComponent } from './pages/accueil/slide/slide.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
