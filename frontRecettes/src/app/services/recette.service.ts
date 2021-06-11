372c9d24542d3cad83d4158e4b443a79b83d6f6c
HaroldWalter <harold11walter@gmail.com>
2021-06-11 14:52:50 +0200

    appelle json

--- frontRecettes/src/app/services/recette.service.ts
+++ frontRecettes/src/app/services/recette.service.ts
@@ -7,21 +7,61 @@ import { Materiel } from '../modeles/materiel';
 import { Recette } from '../modeles/recette';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecetteService {
-  private tabRecettes :Recette[];
-  constructor(private http : HttpClient) { 
-this.http.get("https://api-recettes.herokuapp.com/recettes").toPromise().then(
-  data => {
+  private tabRecettes: Recette[] =[];
 
-  },
-  err => {
-    
-  }
-);
+  constructor(private http: HttpClient) {
+    this.http
+      .get('https://api-recettes.herokuapp.com/recettes')
+      .toPromise()
+      .then(
+        (data: any) => {
+          for (let r of data) {
+            let etapesRecette: Etapes[] = [];
+            for (let e of r.etapes)
+              etapesRecette.push(
+                new Etapes(e.ordre, e.description, e.obligatoire)
+              );
 
-    this.tabRecettes = [
+            let ingredientsRecette: Ingredients[] = [];
+            for (let i of r.ingredients)
+              ingredientsRecette.push(
+                new Ingredients(i.nom, i.quantite, i.unite, i.obligatoire)
+              );
+
+            let materielsRecette: Materiel[] = [];
+            for (let m of r.materiels)
+              materielsRecette.push(
+                new Materiel(m.nom, m.quantite, m.obligatoire)
+              );
+
+            this.tabRecettes.push(
+              new Recette(
+                r.id,
+                r.nom,
+                new Indicateurs(
+                  r.indicateurs.temps_cuisson,
+                  r.indicateurs.temps_preparation,
+                  r.indicateurs.temps_vaiselle,
+                  r.indicateurs.difficulte,
+                  r.indicateurs.cout,
+                  r.indicateurs.nombre_personnes
+                ),
+                r.materielsRecette,
+                r.ingredientsRecette,
+                r.etapesRecette,
+                r.favoris,
+                r.imageUrl
+              )
+            );
+          }
+        },
+        (err) => {}
+      );
+
+    /*    this.tabRecettes = [
       new Recette(
         "78qds478s",
         'Tarte aux pommes',
@@ -79,14 +119,14 @@ this.http.get("https://api-recettes.herokuapp.com/recettes").toPromise().then(
         'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Tarte_normande.jpg/800px-Tarte_normande.jpg'
       ))
     }, 4000
-    )
+    ) */
   }
 
   getRecettes() {
     return this.tabRecettes;
   }
 
-  getRecette(identifiant: string) : Recette | undefined {
-    return this.tabRecettes.find(r => r.id == identifiant);
+  getRecette(identifiant: string): Recette | undefined {
+    return this.tabRecettes.find((r) => r.id == identifiant);
   }
 }
